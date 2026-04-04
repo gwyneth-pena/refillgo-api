df860f9bc2ece932375cfa8cb17535925bd7d24a
Gwen <gwen@Gwens-MacBook-Air.local>
2026-04-04 16:01:26 +0800

    Added email notif upon account creation

--- config.py
+++ config.py
@@ -8,4 +8,13 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
+#mail variables
+MAIL_USER = os.getenv("MAIL_USER")
+MAIL_PASS = os.getenv("MAIL_PASS")
+MAIL_SERVER = os.getenv("MAIL_SERVER")
+MAIL_PORT = os.getenv("MAIL_PORT")
+MAIL_FROM = os.getenv("MAIL_FROM")
+
+ENV = os.getenv("ENV", "development")
 SECRET_KEY = os.getenv("SECRET_KEY")
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
--- v1/endpoints/users.py
+++ v1/endpoints/users.py
@@ -1,19 +1,26 @@
+from config import FRONTEND_URL
 from fastapi import APIRouter, Response
 from fastapi.params import Depends
+from shared.email import send_email
 from sqlalchemy.orm import Session
 from db import get_db
 from modules.users.schemas import UserCreateSchema, UserLoginSchema
 from modules.users.services import add_user, authenticate_user
 from shared.utils import validation_error
+from fastapi import BackgroundTasks
 
 
 router = APIRouter()
 
 @router.post("/")
-def create_user(payload: UserCreateSchema, response: Response, db: Session = Depends(get_db)):
+def create_user(payload: UserCreateSchema, response: Response,  background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     user = add_user(payload, db, response)
 
     if user:
+        background_tasks.add_task(send_email, [user['email']], 'Welcome to RefillGo', 'welcome.html', {
+            'login_link': f'{FRONTEND_URL}/login'
+        })
+    
         response.status_code = 201
         return {
             'message': 'User created successfully.',
